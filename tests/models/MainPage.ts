eeb65e63905b0c4740448f29400e54781371f7bd
Виктория <v.dorzhieva@bnovo.ru>
2025-07-20 20:38:50 +0300

    use POM

---
+++ tests/models/MainPage.ts
@@ -0,0 +1,153 @@
+import test, { expect, Locator, Page } from '@playwright/test';
+
+interface Elements {
+  locator: (page: Page) => Locator;
+  name: string;
+  text?: string;
+  attribute?: {
+    type: string;
+    value: string;
+  };
+}
+
+export class MainPage {
+  readonly page: Page;
+  readonly elements: Elements[];
+
+  constructor(page: Page) {
+    this.page = page;
+    this.elements = [
+      {
+        locator: (page: Page): Locator =>
+          page.getByRole('link', { name: 'Playwright logo Playwright' }),
+        name: 'Playwright logo Link',
+        text: 'Playwright',
+        attribute: {
+          type: 'href',
+          value: '/',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByRole('link', { name: 'Docs' }),
+        name: 'Docs Link',
+        text: 'Docs',
+        attribute: {
+          type: 'href',
+          value: '/docs/intro',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByRole('link', { name: 'API' }),
+        name: 'API Link',
+        text: 'API',
+        attribute: {
+          type: 'href',
+          value: '/docs/api/class-playwright',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByRole('button', { name: 'Node.js' }),
+        name: 'Node.js button',
+        text: 'Node.js',
+      },
+      {
+        locator: (page: Page): Locator => page.getByRole('link', { name: 'Community' }),
+        name: 'Community Link',
+        text: 'Community',
+        attribute: {
+          type: 'href',
+          value: '/community/welcome',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByLabel('GitHub repository'),
+        name: 'GitHub icon',
+        attribute: {
+          type: 'href',
+          value: 'https://github.com/microsoft/playwright',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByLabel('Discord server'),
+        name: 'Discord icon',
+        attribute: {
+          type: 'href',
+          value: 'https://aka.ms/playwright/discord',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByLabel('Switch between dark and light'),
+        name: 'Lightmode icon',
+      },
+      {
+        locator: (page: Page): Locator => page.getByLabel('Search (Command+K)'),
+        name: 'Search Link',
+      },
+      {
+        locator: (page: Page): Locator =>
+          page.getByRole('heading', { name: 'Playwright enables reliable' }),
+        name: 'Title',
+        text: 'Playwright enables reliable end-to-end testing for modern web apps.',
+      },
+      {
+        locator: (page: Page): Locator => page.getByRole('link', { name: 'Get started' }),
+        name: 'Get started button',
+        text: 'Get started',
+        attribute: {
+          type: 'href',
+          value: '/docs/intro',
+        },
+      },
+    ];
+  }
+  async openMainPage() {
+    await this.page.goto('https://playwright.dev');
+  }
+  async checkElementsVisability() {
+    for (const { locator, name } of this.elements) {
+      await test.step(`Проверка отображения элементов ${name}`, async () => {
+        await expect.soft(locator(this.page)).toBeVisible();
+      });
+    }
+  }
+  async checkElementsText() {
+    for (const { locator, name, text } of this.elements) {
+      if (text) {
+        await test.step(`Проверка названия элементов ${name}`, async () => {
+          await expect.soft(locator(this.page)).toContainText(text);
+        });
+      }
+    }
+  }
+  async checkElementsHrefAttribute() {
+    for (const { locator, name, attribute } of this.elements) {
+      if (attribute) {
+        await test.step(`Проверка атрибутов href элементf ${name}`, async () => {
+          await expect.soft(locator(this.page)).toHaveAttribute(attribute?.type, attribute?.value);
+        });
+      }
+    }
+  }
+  async clickSwitchLightModeIcon() {
+    await this.page.getByLabel('Switch between dark and light').click();
+  }
+  async checkDataThemeAttrubuteValue() {
+    await expect.soft(this.page.locator('html')).toHaveAttribute('data-theme', 'light');
+  }
+  async setLightMode() {
+    await this.page.evaluate(() => {
+      document.querySelector('html')?.setAttribute('data-theme', 'light');
+    });
+  }
+  async setDarkMode() {
+    await this.page.evaluate(() => {
+      document.querySelector('html')?.setAttribute('data-theme', 'dark');
+    });
+  }
+  async checkLayoutWithLightMode() {
+    await expect(this.page).toHaveScreenshot(`pageWithLightMoge.png`);
+  }
+  async checkLayoutWithDarkMode() {
+    await expect(this.page).toHaveScreenshot(`pageWithDarkMoge.png`);
+  }
+}
